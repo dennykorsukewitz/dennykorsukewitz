6b65cf7065a5651bd76085e52eefe3adb3452f6b
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2024-03-08 22:49:07 +0100

    Tidied.

--- .github/workflows/metrics/npm.sh
+++ .github/workflows/metrics/npm.sh
@@ -16,7 +16,7 @@ CURRENT_JSON_DAILY=$(jq . ./.github/metrics/data/npm-daily.json)
 CURRENT_JSON_TOTAL=$(jq . ./.github/metrics/data/npm-total.json)
 
 # Get yesterday's date
-TIMESTAMP=$(date -u -v -1d +"%Y-%m-%d")
+TIMESTAMP=$(date -u -d '1 day ago' +"%Y-%m-%d")
 
 # TIMESTAMP="2024-03-07"
 START_TIMESTAMP=$TIMESTAMP
