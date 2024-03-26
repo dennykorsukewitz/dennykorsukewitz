7bc0599e26cc47161576f1fcc6dc1c470d8840b9
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2024-03-26 22:31:30 +0100

    Tidied github-webhooks.sh.

--- .github/scripts/github-webhooks.sh
+++ .github/scripts/github-webhooks.sh
@@ -21,87 +21,87 @@ for REPOSITORY in "${REPOSITORIES[@]}"; do
     done
 
     echo "all"
-    RESPONSE=$(gh api -X POST repos/"$OWNER"/"$REPOSITORY"/hooks --input <(cat <<< '{
-    "name": "web",
-    "active": true,
-    "events": [
-        "branch_protection_configuration",
-        "branch_protection_rule",
-        "check_run",
-        "check_suite",
-        "code_scanning_alert",
-        "commit_comment",
-        "create",
-        "custom_property_values",
-        "delete",
-        "dependabot_alert",
-        "deploy_key",
-        "deployment",
-        "deployment_status",
-        "discussion",
-        "discussion_comment",
-        "fork",
-        "gollum",
-        "issue_comment",
-        "issues",
-        "label",
-        "member",
-        "merge_group",
-        "meta",
-        "milestone",
-        "package",
-        "page_build",
-        "ping",
-        "project_card",
-        "project",
-        "project_column",
-        "public",
-        "pull_request",
-        "pull_request_review_comment",
-        "pull_request_review",
-        "pull_request_review_thread",
-        "push",
-        "registry_package",
-        "repository_advisory",
-        "repository",
-        "repository_import",
-        "repository_ruleset",
-        "repository_vulnerability_alert",
-        "secret_scanning_alert",
-        "secret_scanning_alert_location",
-        "security_and_analysis",
-        "status",
-        "team_add",
-        "watch",
-        "workflow_job",
-        "workflow_run"
+    RESPONSE=$(gh api -X POST repos/"$OWNER"/"$REPOSITORY"/hooks --input <(cat <<< "{
+    'name': 'web',
+    'active': true,
+    'events': [
+        'branch_protection_configuration',
+        'branch_protection_rule',
+        'check_run',
+        'check_suite',
+        'code_scanning_alert',
+        'commit_comment',
+        'create',
+        'custom_property_values',
+        'delete',
+        'dependabot_alert',
+        'deploy_key',
+        'deployment',
+        'deployment_status',
+        'discussion',
+        'discussion_comment',
+        'fork',
+        'gollum',
+        'issue_comment',
+        'issues',
+        'label',
+        'member',
+        'merge_group',
+        'meta',
+        'milestone',
+        'package',
+        'page_build',
+        'ping',
+        'project_card',
+        'project',
+        'project_column',
+        'public',
+        'pull_request',
+        'pull_request_review_comment',
+        'pull_request_review',
+        'pull_request_review_thread',
+        'push',
+        'registry_package',
+        'repository_advisory',
+        'repository',
+        'repository_import',
+        'repository_ruleset',
+        'repository_vulnerability_alert',
+        'secret_scanning_alert',
+        'secret_scanning_alert_location',
+        'security_and_analysis',
+        'status',
+        'team_add',
+        'watch',
+        'workflow_job',
+        'workflow_run'
     ],
-    "config": {
-        "url": "${{ secrets.DISCORD_WEBHOOK_ANY }}",
-        "content_type": "json"
+    'config': {
+        'url': '${{ secrets.DISCORD_WEBHOOK_ANY }}',
+        'content_type': 'json'
     }
-}')) > /dev/null 2>&1
+}")) > /dev/null 2>&1
 
     echo "star"
-    RESPONSE=$(gh api -X POST repos/"$OWNER"/"$REPOSITORY"/hooks --input <(cat <<< '{
-        "name": "web",
-        "active": true,
-        "events": ["star", "watch"],
-        "config": {
-            "url": "${{ secrets.DISCORD_WEBHOOK_STAR }}": "json"
+    RESPONSE=$(gh api -X POST repos/"$OWNER"/"$REPOSITORY"/hooks --input <(cat <<< "{
+        'name': 'web',
+        'active': true,
+        'events': ['star', 'watch'],
+        'config': {
+            'url': '${{ secrets.DISCORD_WEBHOOK_STAR }}': 'json'
         }
-    }')) > /dev/null 2>&1
+    }")) > /dev/null 2>&1
 
     echo "release"
-    RESPONSE=$(gh api -X POST repos/"$OWNER"/"$REPOSITORY"/hooks --input <(cat <<< '{
-        "name": "web",
-        "active": true,
-        "events": ["release"],
-        "config": {
-            "url": "${{ secrets.DISCORD_WEDISCORD_WEBHOOK_RELEASEBHOOK_STAR }}",
-            "content_type": "json"
+    RESPONSE=$(gh api -X POST repos/"$OWNER"/"$REPOSITORY"/hooks --input <(cat <<< "{
+        'name': 'web',
+        'active': true,
+        'events': ['release'],
+        'config': {
+            'url': '${{ secrets.DISCORD_WEDISCORD_WEBHOOK_RELEASEBHOOK_STAR }}',
+            'content_type': 'json'
         }
-    }')) > /dev/null 2>&1
+    }")) > /dev/null 2>&1
 
     echo "$RESPONSE"
 
