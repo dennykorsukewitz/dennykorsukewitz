b2f9a6f7d8b52b965344169c0956a70bd205276a
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2024-04-08 23:55:50 +0200

    Update github.sh

--- .github/workflows/metrics/github.sh
+++ .github/workflows/metrics/github.sh
@@ -21,11 +21,6 @@ for REPOSITORY in "${REPOSITORIES[@]}"; do
 
     mapfile -t STARGAZERS < <(gh api -H "Accept: application/vnd.github.v3.star+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/"$OWNER"/"$REPOSITORY"/stargazers --jq '.[]')
 
-    if [ -z "$STARGAZERS" ] ; then
-      echo -e "❌ No STARGAZERS received."
-      exit 1
-    fi
-
     if [ -z "${STARGAZERS[0]}" ] ; then
       continue;
     fi
