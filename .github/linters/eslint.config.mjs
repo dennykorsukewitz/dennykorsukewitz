a17a498e00a75224fd6bdbb5f3160cc4e039d660
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2026-04-04 20:39:03 +0200

    Added .github/linters/eslint.config.mjs

---
+++ .github/linters/eslint.config.mjs
@@ -0,0 +1,42 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import nPlugin from 'eslint-plugin-n';
+
+export default [
+    {
+        ignores: ['out/**', 'dist/**', '**/*.d.ts'],
+    },
+    {
+        files: ['src/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            n: nPlugin,
+        },
+        rules: {
+            'n/no-missing-import': [
+                'error',
+                {
+                    allowModules: ['vscode'],
+                },
+            ],
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'import',
+                    format: ['camelCase', 'PascalCase'],
+                },
+            ],
+            curly: 'warn',
+            eqeqeq: 'warn',
+            'no-throw-literal': 'warn',
+            semi: ['warn', 'always'],
+        },
+    },
+];
